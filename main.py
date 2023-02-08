61408a45bedc590c105023299fa513326c47f7ee
MrSabin <sabin.21011986@gmail.com>
2023-02-08 21:31:07 +0300

    Downloaded books from tululu.org

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
+from pathlib import Path
 
+path = "books"
+Path(path).mkdir(parents=True, exist_ok=True)
 
-url = "https://tululu.org/txt.php?id=32168"
+for book_id in range(1, 11):
+    url = f"https://tululu.org/txt.php?id={book_id}"
+    name = f"{book_id}.txt"
 
-response = requests.get(url)
-response.raise_for_status()
+    response = requests.get(url)
+    response.raise_for_status()
 
-filename = "mars_sands.txt"
-with open(filename, "w") as file:
-    file.write(response.text)
+    filename = Path(path, name)
+    with open(filename, "w") as file:
+        file.write(response.text)
