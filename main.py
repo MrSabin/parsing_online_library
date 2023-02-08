15a5f9befb6dbe9313e7dc263c8893e77c48d7a7
MrSabin <sabin.21011986@gmail.com>
2023-02-09 01:12:27 +0300

    Added check for redirect

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-import requests
+import requests, urllib3
 from pathlib import Path
 
-path = "books"
-Path(path).mkdir(parents=True, exist_ok=True)
 
-for book_id in range(1, 11):
+def download_book(path, book_id):
     url = f"https://tululu.org/txt.php?id={book_id}"
     name = f"{book_id}.txt"
 
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     response.raise_for_status()
+    check_for_redirect(response)
 
     filename = Path(path, name)
     with open(filename, "w") as file:
         file.write(response.text)
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+    else:
+        pass
+
+
+def main():
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    path = "books"
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    for book_id in range(1, 11):
+        try:
+            download_book(path, book_id)
+        except requests.HTTPError:
+            print("File URL is not valid. Skipping to next...")
+            continue
+
+
+if __name__ == "__main__":
+    main()
