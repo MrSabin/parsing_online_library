4eaf022672b2d8f611e522adc731a5a2d16413ab
MrSabin <sabin.21011986@gmail.com>
2023-02-15 09:39:02 +0300

    Added tqdm progressbar

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -3,6 +3,7 @@ from urllib.parse import urljoin
 import requests
 import urllib3
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 
 def parse_page(response):
@@ -17,14 +18,14 @@ def parse_page(response):
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     all_book_urls = []
-    for page_number in range(1, 11):
+    for page_number in tqdm(range(1, 11)):
+
         url = f"https://tululu.org/l55/{page_number}"
         response = requests.get(url, verify=False)
         response.raise_for_status()
 
         page_book_urls = parse_page(response)
         all_book_urls.extend(page_book_urls)
-    print(all_book_urls)
 
 
 if __name__ == "__main__":
