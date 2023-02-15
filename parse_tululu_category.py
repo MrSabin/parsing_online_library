bdfd2c08091413ca3994d3bcb0e85e32ddc1efd6
MrSabin <sabin.21011986@gmail.com>
2023-02-15 09:32:22 +0300

    Parsed urls from first 10 pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -11,16 +11,20 @@ def parse_page(response):
         content.a["href"] for content in soup.select("#content .d_book")
     ]
     book_urls = [urljoin(response.url, book_id) for book_id in book_ids]
-    print(book_urls)
+    return book_urls
 
 
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    url = "https://tululu.org/l55/"
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
+    all_book_urls = []
+    for page_number in range(1, 11):
+        url = f"https://tululu.org/l55/{page_number}"
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
 
-    parse_page(response)
+        page_book_urls = parse_page(response)
+        all_book_urls.extend(page_book_urls)
+    print(all_book_urls)
 
 
 if __name__ == "__main__":
