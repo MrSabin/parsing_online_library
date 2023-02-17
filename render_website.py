8158d5dfa66bc5b683cdedd5cec2c7dddd1130ae
MrSabin <sabin.21011986@gmail.com>
2023-02-17 21:21:24 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -14,6 +14,7 @@ books_dump = json.loads(books_json)
 books_on_page = 20
 columns_on_page = 2
 chunked_dump = list(chunked(books_dump, books_on_page))
+pages_count = len(chunked_dump)
 
 
 def rebuild():
@@ -22,7 +23,11 @@ def rebuild():
     Path(pages_folder).mkdir(parents=True, exist_ok=True)
     for page_number, chunk in enumerate(chunked_dump, 1):
         books = list(chunked(chunk, columns_on_page))
-        rendered_page = template.render(books_dump=books)
+        rendered_page = template.render(
+            books_dump=books,
+            pages_count=pages_count,
+            page_number=page_number,
+        )
         filename = Path(pages_folder, f"index{page_number}.html")
         with open(filename, "w", encoding="utf8") as file:
             file.write(rendered_page)
