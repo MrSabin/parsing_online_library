76e1d7254f3a9753d86414d131baf471d7159262
MrSabin <sabin.21011986@gmail.com>
2023-02-18 02:59:30 +0300

    Changed file structure

--- render_website.py
+++ render_website.py
@@ -6,27 +6,16 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
-parser = argparse.ArgumentParser(
-    description="Script for science fiction books download"
-)
-parser.add_argument(
-    "--json_path",
-    help="Folder with JSON file",
-    default="json/books_dump.json",
-)
-args = parser.parse_args()
-env = Environment(
-    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
-)
-with open(args.json_path, "r") as file:
-    books_dump = json.load(file)
-book_cards_on_page = 20
-columns_on_page = 2
-chunked_book_cards = list(chunked(books_dump, book_cards_on_page))
-pages_count = len(chunked_book_cards)
 
-
-def rebuild():
+def rebuild(books_dump):
+    env = Environment(
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html", "xml"]),
+    )
+    book_cards_on_page = 20
+    columns_on_page = 2
+    chunked_book_cards = list(chunked(books_dump, book_cards_on_page))
+    pages_count = len(chunked_book_cards)
     template = env.get_template("template.html")
     pages_folder = "pages/"
     Path(pages_folder).mkdir(parents=True, exist_ok=True)
@@ -42,7 +31,24 @@ def rebuild():
             file.write(rendered_page)
 
 
-rebuild()
-server = Server()
-server.watch("template.html", rebuild)
-server.serve(root=".")
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script for science fiction books download"
+    )
+    parser.add_argument(
+        "--json_path",
+        help="Folder with JSON file",
+        default="json/books_dump.json",
+    )
+    args = parser.parse_args()
+    with open(args.json_path, "r") as file:
+        books_dump = json.load(file)
+
+    rebuild(books_dump)
+    server = Server()
+    server.watch("template.html", rebuild(books_dump))
+    server.serve(root=".")
+
+
+if __name__ == "__main__":
+    main()
