1e6ddc7ee1f5671145f04722c513ad1da1912ab7
MrSabin <sabin.21011986@gmail.com>
2023-02-17 15:56:07 +0300

    Integrated livereload

--- render_website.py
+++ render_website.py
@@ -1,21 +1,25 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
 env = Environment(
     loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
 )
-
 with open("json/books_dump.json", "r") as file:
     books_json = file.read()
 books_dump = json.loads(books_json)
 
-template = env.get_template("template.html")
-rendered_page = template.render(books_dump=books_dump)
 
-with open("index.html", "w", encoding="utf8") as file:
-    file.write(rendered_page)
+def rebuild():
+    template = env.get_template("template.html")
+    rendered_page = template.render(books_dump=books_dump)
+    with open("index.html", "w", encoding="utf8") as file:
+        file.write(rendered_page)
+    print("Site rebuilt")
 
-server = HTTPServer(("", 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+
+rebuild()
+server = Server()
+server.watch("template.html", rebuild)
+server.serve(root=".")
